bd55139753d1974686fbf089b26fa0581b28ad9b
SimasDei <simondeiman@gmail.com>
2019-02-18 19:31:06 +0200

    npm i react modules, get project ready for webpack

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Users dude!</h1>
+        dab \o\
+      </div>
+    );
+  }
+}
+
+export default Users;
