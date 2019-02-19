b57eed35359d1c2eef6e8002ba8a1fe1dadaddd8
SimasDei <simondeiman@gmail.com>
2019-02-19 14:16:10 +0200

    add babel config and loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,14 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
   }
 };
