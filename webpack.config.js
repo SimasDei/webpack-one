2bf73454f92b2e0f0d71ac5c0fe7a6e84f2930a2
SimasDei <simondeiman@gmail.com>
2019-02-19 14:22:49 +0200

    add css loader, preloader and auto prefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: './src/index.js',
@@ -16,6 +17,32 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => {
+                autoprefixer({
+                  browsers: ['> 1%', 'last 2 versions']
+                });
+              }
+            }
+          }
+        ]
       }
     ]
   }
